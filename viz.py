672c16a4ae1aabab83a4c55cc06fa61c727e462d
fletchgraham <sfletchgraham@gmail.com>
2021-01-29 10:27:30 -0500

    square color test cli

---
+++ viz.py
@@ -0,0 +1,23 @@
+from random import randint
+
+answer = None
+
+fs = int(ord(input('file start? ')) - 96)
+fe = int(ord(input('file end? ')) - 96)
+rs = int(input('rank start? '))
+re = int(input('rank end? '))
+
+score = 0
+while not answer == 'q':
+    rank = randint(rs, re)
+    filenum = randint(fs, fe)
+    color = 'b'
+    if (rank + filenum) % 2:
+        color = 'w'
+    answer = input(f'{chr(filenum + 96)}{rank} color? ')
+    if answer == color:
+        print('correct!')
+        score += 1
+    else:
+        print(f'game over. score: {score}')
+        quit()
